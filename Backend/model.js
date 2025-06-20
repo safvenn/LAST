f8cc0078eea8c33d1a3bc5dc88c80b29cdc64ce9
safvenn <mkdsafwan4@gmail.com>
2025-06-20 23:19:06 +0400

    Delete Backend directory

--- Backend/model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PostSchema = new mongoose.Schema({
-  title: { type: String },
-  category: { type: String },
-  image: { type: String },
-});
-
-module.exports = mongoose.model('Post', PostSchema);
